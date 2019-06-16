1d88397557fe7622b0b01128baccfb6cd7b36681
Sidharath <sid.aggarwal.31@gmail.com>
2019-06-15 22:49:43 -0400

    Fixed the list view to show the items

--- app/src/main/java/com/example/owner/musicplayer/HomeFragment.java
+++ app/src/main/java/com/example/owner/musicplayer/HomeFragment.java
@@ -1,6 +1,6 @@
 package com.example.owner.musicplayer;
-
-
+import android.app.Application;
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,65 +9,42 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends AppCompatActivity {
-
-        private MediaPlayer mediaPlayer;
-        private MediaPlayer.OnCompletionListener mCompleteListener = new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mediaPlayer) {
-                releaseMediaPlayer();
-            }
-        };
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.fragment_home2);
-            final ArrayList<Song> songs = new ArrayList<Song>();
-            songs.add(new Song("Wake up Sid", "Ed Sheeran", "3:21", R.raw.wake_up_sid, R.drawable.music_image));
-            songs.add(new Song("Wake up Sid", "Ed Sheeran", "3:21", R.raw.wake_up_sid, R.drawable.music_image));
-            // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
-            // adapter knows how to create list items for each item in the list.
-            SongAdapter adaptor = new SongAdapter(this, songs);
-            ListView listView = (ListView) findViewById(R.id.list);
-
-            listView.setAdapter(adaptor);
-
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    Song song = songs.get(position);
-
-                    releaseMediaPlayer();
-
-                    mediaPlayer = MediaPlayer.create(HomeFragment.this, song.getAudioResourceId());
-
-                    mediaPlayer.start();
-
-                    mediaPlayer.setOnCompletionListener(mCompleteListener);
-                }
-            });
-        }
-
-        @Override
-        public void onStop() {
-            super.onStop();
-            releaseMediaPlayer();
-        }
+public class HomeFragment extends Fragment {
 
 
-        private void releaseMediaPlayer() {
-            if (mediaPlayer != null) {
-                mediaPlayer.release();
-                mediaPlayer = null;
-            }
-        }
+
+    public HomeFragment() {
+        // Required empty public constructor
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home2, container, false);
+
+        final ArrayList<Song> songs = new ArrayList<>();
+        songs.add(new Song("Wake up Sid.mp3", "Ed Sheeran", "3:21", R.raw.wake_up_sid, R.drawable.music_image));
+
+
+
+        SongAdapter adapter = new SongAdapter(getContext(), songs);
+
+        ListView listView = (ListView) view.findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
+        // Inflate the layout for this fragment
+
+        return view;
+    }
+
+
+
+}
